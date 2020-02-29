47af9fb194b7d63b9740c9fe3b162b46cea55cb6
gwilliams <1708965@brunel.ac.uk>
2020-02-29 21:30:12 +0000

    bug fixes/database recording modification

--- chat.py
+++ chat.py
@@ -1,23 +1,26 @@
 import json
 import datetime
 import mysql.connector
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from word2vec import w2v
 class chatBot:
     def __init__(self):
         self.importData()
         self.topicChooser = w2v()
         self.conDB()
-        cusPrint("hi, what can I help you with?")
+        self.cusPrint("hi, what can I help you with?")
 
 #internal states
     function=None
-    topic = "none"
+    topic = [None,None]
     state = "OPEN"
     running = True
     topTopics=[]
-    lastMsg=None
-    info=[]
+    lastMsg=""
+    MsgS=[]
 #data
+    info=[]
     topicChooser=None
     topics = []
     checks = []
@@ -32,7 +35,6 @@ class chatBot:
         elif self.state == "FUNC":
             if self.function=="what":
                 self.func_question()
-                self.clr()
             elif self.function=="maintenance":
                 self.func_maintenance()
                 self.clr()
@@ -45,120 +47,161 @@ class chatBot:
 
     def clr(self):
         self.state="OPEN"
-        self.topic=None
+        self.topic=[None,None]
         self.function=None
-        cusPrint("Is there anything else I can help you with? ")
+        self.topTopics=[]
+        self.cusPrint("What else can I help you with? ")
 
     def state_conf(self):
-        uInp = cusInput("are you having a problem with "+ self.topic+"?")
-        temp = self.topics
-        temp.append(["yes",["yes"]])
-        temp.append(["no",["no"]])
-        res =  self.topicChooser.topic_select(temp,uInp)
-        if  res[0][0]== "yes":
-            self.state = "TOPIC"
-        if res[0][0]=="no":
-            del self.topTopics[0]
-            if len(self.topTopics)>0:
-                self.topic = self.topTopics[0][0]
+        if len(self.topTopics)>=1:
+
+            uInp = self.cusInput("are you having a problem with "+ self.topic[0]+"?")
+            temp = self.topics
+            temp.append(["yes",["yes"]])
+            temp.append(["no",["no"]])
+            res =  self.topicChooser.topic_select_V2(temp,uInp, 0.4)
+            if  res[0][0]== "yes":
+                self.state = "TOPIC"
+            elif res[0][0]=="no":
+                del self.topTopics[0]
+                if len(self.topTopics)>0:
+                    self.topic = self.topTopics[0]
+                else:
+                    self.state="OPEN"
+                    self.topic=[None,None]
+                    self.cusPrint("sorry i don't think i know what you mean, what else may i help you with?")
             else:
-                self.state="OPEN"
-                self.topic="none"
+                self.troubleshooter_detection(uInp)
+        else:
+            self.state = "OPEN"
+            self.cusPrint("sorry i don't think i know what you mean, what else may i help you with?")
+
+
 
     def state_open(self):
-        uin = cusInput('')
+        uin = self.cusInput('')
         #funcitionality checks (if the user wants to submit maintinance request/complaint/ask question)
-        #func=self.topicChooser.func_select(["what"],uin)
-        #if func[0] =="what":
-        #    self.setFunction(func[0])
-        #   return
-        #else:
-        #    functions=["maintenance", "complain", "feedback"]
-        #    func = self.topicChooser.func_select(functions, uin)
-        #    if func[0] != None:
-        #        self.setFunction(func[0])
-        #        return
 
-        #catch all: checks if problem matches troubleshooting backend
-        self.topTopics = self.topicChooser.topic_select_V2(self.topics,uin)
-        self.topic = self.topTopics[0][0]
-        if self.topic == "None":
-            cusPrint("GoodBye!")
-            self.running = False
+        functions = ["maintenance", "complain", "feedback"]
+        tInput = word_tokenize(uin)
+        tremStopwInput = self.topicChooser.remStopWords(tInput)
+        stemStopInp = self.topicChooser.stem_txt(tremStopwInput)
+        func = self.topicChooser.func_select(functions, stemStopInp,0.35)
+        if func[0] != None:
+            self.setFunction(func[0])
+            return
+        #question detections, detects similar interigative words
+        func=self.topicChooser.func_select(["what","where"],tInput,0.6)
+        if func[0] =="what" or func[0] =="where":
+            self.setFunction("what")
+            return
         else:
-            self.state = "CONF"
-
+            self.troubleshooter_detection(uin)
     def state_topic(self):
         fResp = ""
         for i in self.checks:
-            if i[3] == self.topic:
-                inp = cusInput(i[0])
+            if i[3] == self.topic[0]:
+                inp = self.cusInput(i[0])
                 temp=self.topics
                 temp.append(["yes",["yes"]])
                 temp.append(["no",["no"]])
-                proc = self.topicChooser.topic_select(temp,inp)[0][0]
+                proc = self.topicChooser.topic_select_V2(temp,inp, 0.6)[0][0]
                 if i[2] == True:
                     if proc == "no":
                         txt = i[1]
                     elif proc == "yes":
                         txt = i[4]
                     if txt == "Record":
-
+                        self.func_maintenance()
                         break
                     if txt != "":
-                        cusPrint(txt)
-                        conf = cusInput("Did that fix the problem?")
+                        self.cusPrint(txt)
+                        conf = self.cusInput("Did that fix the problem?")
                         if conf == "yes":
-                            fResp = "Great! is there anything else I can help you with?"
+                            fResp = "Great!"
+
                             break
                     i[2] = False
         if fResp == "":
-            fResp = "I'm sorry I couldn't help you with your problem"
+            fResp = "I'm sorry. "
             self.func_maintenance()
         else:
             self.state = "OPEN"
-            self.topic = ""
-        cusPrint(fResp)
+            self.topic = ["",None]
+        self.cusPrint(fResp)
+        self.clr()
+
+    def troubleshooter_detection(self, uin):
+        uin = word_tokenize(uin)
+        uin = self.topicChooser.remStopWords(uin)
+        urgent = self.urgencyCheck()
+        self.topTopics = self.topicChooser.topic_select(self.topics,uin,0.2)
+        if(self.topTopics!=[]):
+            self.topic = self.topTopics[0]
+            if self.topic[0] == "None":
+                cusPrint("GoodBye!")
+                self.running = False
+            elif self.topic[1]>0.3:
+                self.state = "CONF"
+            else:
+                self.cusPrint("Sorry, I didn't understand that")
+                self.clr()
+        else:
+            self.cusPrint("Sorry, I didn't understand that")
+            self.clr()
+
 
     def func_maintenance(self):
-        inp = cusInput("would you like to submit a maintenance request?")
-        if inp == "yes":
+        inp = self.cusInput("would you like to submit a maintenance request?")
+        temp = self.topics
+        temp.append(["yes",["yes"]])
+        temp.append(["no",["no"]])
+        res = self.topicChooser.topic_select_V2(temp, inp, 0.5)
+        urgent = self.urgencyCheck()
+        print(urgent)
+        if res[0][0] == "yes":
             global conm, cur
-            cusPrint("ok, I just need to clarify a few things.")
-            location = cusInput("can you provide a general description of the problem")
-            address = cusInput("what is the address of the property?")
-            name = cusInput("and your full name please?")
-            number = cusInput("and finally what is your phone number so we can contact you?")
-            sql = "INSERT INTO maintenance (loc, addr, name, num, topic, CID) VALUES (%s,%s,%s,%s,%s,%s)"
-            val = (location, address, name, number, self.topic, "1")
+            self.cusPrint("ok, I just need to clarify a few things.")
+            location = self.cusInput("can you provide a general description of the problem")
+            address = self.cusInput("what is the address of the property?")
+            name = self.cusInput("and your full name please?")
+            number = self.cusInput("and finally what is your phone number so we can contact you?")
+            sql = "INSERT INTO maintenance (loc, addr, name, num, topic, CID, emergency, MID, date) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            val = (location, address, name, number, self.topic[0], CID,urgent,None, datetime.datetime.now())
             cur.execute(sql, val)
             conn.commit()
-            cusPrint(
+            self.cusPrint(
                 "Thank you, I have created a log of your request, we will be in contact with you as soon as possible")
             self.state = "OPEN"
             self.topic = ""
 
     def func_question(self):
-        global lastMsg
-        data = self.topicChooser.topic_select_V2(self.info,lastMsg)
-        print(data[0][0])
-        self.state=="OPEN"
-        self.function==None
+        msg = word_tokenize(self.lastMsg)
+        msg = self.topicChooser.remStopWords(msg)
+        data = self.topicChooser.topic_select(self.info,msg, 0.4)
+        if len(data)>0:
+            if data[0][1] >0.4:
+                print(data[0][0])
+                self.state="OPEN"
+            else:
+                self.troubleshooter_detection(self.lastMsg)
+        else:
+            self.troubleshooter_detection(self.lastMsg)
 
     def func_complaint(self):
-        inp = cusInput("would you like to me to submit an official complaint/feedback report for you")
+        inp = self.cusInput("would you like to me to submit an official complaint/feedback report for you")
         if inp == "yes":
             global conm, cur
-            cusPrint("ok, I just need to clarify a few things.")
-            address = cusInput("what is the address of the property?")
-            name = cusInput("and your full name please?")
-            number = cusInput("and finally what is your phone number so we can contact you?")
-            desc = cusInput("Thank you. now finally what would you like to report?")
+            self.cusPrint("ok, I just need to clarify a few things.")
+            address = self.cusInput("what is the address of the property?")
+            name = self.cusInput("and your full name please?")
+            number = self.cusInput("and finally what is your phone number so we can contact you?")
+            desc = self.cusInput("Thank you. now finally what would you like to report?")
             sql = "INSERT INTO complaint (descr, addr, name, num, topic, CID) VALUES (%s,%s,%s,%s,%s,%s)"
             val = (desc, address, name, number,self.topic, "1")
             #cur.execute(sql, val)
             #conn.commit()
-            cusPrint(
+            self.cusPrint(
                 "Thank you, the report has been created!")
             self.state = "OPEN"
             self.topic = ""
@@ -170,6 +213,9 @@ class chatBot:
             self.state="FUNC"
         elif func =="what":
             self.function = "what"
+            self.state = "FUNC"
+        elif func == "complain":
+            self.function = "complain"
             self.state = "FUNC"
         elif func == "complain":
             self.function = "complain"
@@ -191,33 +237,53 @@ class chatBot:
             d=data['info'][i]
             temp = [d['data'],d['tags']]
             self.info.append(temp)
-
+    def urgencyCheck(self):
+        str=""
+        for m in self.MsgS:
+            str+=m
+        str = word_tokenize(str)
+        str = self.topicChooser.remStopWords(str)
+        out = self.topicChooser.func_select(["urgent"],str,0.4)
+        if out[0] =="urgent":
+            return True
+        else:
+            return False
     def conDB(self):
         global conn, cur
         conn = mysql.connector.connect(host="localhost", user="root", passwd="Gmysql8952w", database="bot")
         cur = conn.cursor()
 
-def cusInput(*args):
-    if args != None:
-        global conn,cur, lastMsg
+    def cusInput(self,msg):
+        if msg != "":
+            global conn,cur
+            sql = "INSERT INTO clog (mID, bot, msg, date, CID) VALUES(%s,%s,%s,%s,%s)"
+            val = ("1", "1", msg, datetime.datetime.now(), "1")
+
+            f = open("chatLog.txt", "a")
+            f.write(msg + "\n")
+
+            #cur.execute(sql, val)
+            #conn.commit()
+            usrIn = input(msg)
+            self.MsgS.append(usrIn)
+            self.lastMsg = usrIn
+
+        else:
+            usrIn = input()
+            self.MsgS.append(usrIn)
+            self.lastMsg = usrIn
+        return usrIn
+
+    def cusPrint(self, msg):
+        global conn, cur
         sql = "INSERT INTO clog (mID, bot, msg, date, CID) VALUES(%s,%s,%s,%s,%s)"
-        val = ("1", "1", args[0], datetime.datetime.now(), "1")
+        val =("1","1",msg,datetime.datetime.now(),"1")
+        f=open("chatLog.txt","a")
+        f.write(msg+"\n")
 
-        cur.execute(sql, val)
-        conn.commit()
-        usrIn = input(args[0])
-        lastMsg = usrIn
-    else:
-        usrIn = input()
-    return usrIn
-
-def cusPrint(msg):
-    global conn, cur
-    sql = "INSERT INTO clog (mID, bot, msg, date, CID) VALUES(%s,%s,%s,%s,%s)"
-    val =("1","1",msg,datetime.datetime.now(),"1")
-    cur.execute(sql, val)
-    conn.commit()
-    print(msg)
+        #cur.execute(sql, val)
+        #conn.commit()
+        print(msg)
 
 bot = chatBot()
 while bot.running:
--- word2vec.py
+++ word2vec.py
@@ -1,44 +1,75 @@
 import gensim
 from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
 import nltk
 #nltk.download('punkt')
+#nltk.download('stopwords')
 class w2v:
     model=None
     def __init__(self):
         self.model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNewsModel.bin',limit=100000, binary=True)
         #self.model = Word2Vec.load("./trained_mod_V2.model")
-    def topic_select(self,topics, uin):
-        tInput = word_tokenize(uin)
-        topSim=[]
+
+    def topic_select(self, topics, uin,thresh):
+        tInput = uin
+        topSim = []
         for topic in topics:
-            similarity=0
+            similarity = 0
             for w in tInput:
                 vectors = self.model.wv
                 for tag in topic[1]:
                     if w in vectors and tag in vectors:
                         sim = self.model.similarity(tag, w)
-                        if sim>similarity and sim>0.2:
+                        if sim > similarity and sim > thresh:
                             similarity = sim
                             topSim.append([topic[0], similarity])
 
         chosenTopic = self.findTop(topSim)
         return chosenTopic
-
-    def topic_select_V2(self, topics, uin):
+    def topic_select_V3(self,topics, uin,thresh):
         tInput = word_tokenize(uin)
+        topSim=[]
+        vectors = self.model.wv
+        for topic in topics:
+            topicSim=0
+            topicN=0
+            for w in tInput:
+                for tag in topic[1]:
+                    if w in vectors and tag in vectors:
+                        sim = self.model.similarity(tag, w)
+                        if sim>thresh:
+                            topicSim+=sim
+                            topicN+=1
+            if topicSim!=0:
+                topSim.append([topic[0],topicSim/topicN])
+
+        chosenTopic = self.findTop(topSim)
+        return chosenTopic
+
+    def stem_txt(self,msg):
+        stemmer = PorterStemmer()
+        t=[]
+        for m in msg:
+            t.append(stemmer.stem(m))
+        return t
+    def topic_select_V2(self, topics, uin, thresh):
+        tInput = word_tokenize(uin)
+        tInput = self.stem_txt(tInput)
         top_av_sim=[]
         vectors = self.model.wv
         for topic in topics:
             total=0
             n=0
             for tag in topic[1]:
+                tag = PorterStemmer().stem(tag)
                 t=0
                 for w in tInput:
                     if w in vectors and tag in vectors:
                         sim = self.model.similarity(tag, w)
-                        if sim>0.2 and sim>t:
-                            t=sim
+                        if sim>thresh:
+                            t=t+sim
                 total+=t
                 n+=1
             if total==0:
@@ -48,18 +79,27 @@ class w2v:
         chosenTopic = self.findTop(top_av_sim)
         return chosenTopic
 
+    def remStopWords(self,txt):
+        stopW = set(stopwords.words('english'))
+        filtered = []
+        for w in txt:
+            if w not in stopW:
+                filtered.append(w)
+        return filtered
 
-
-    def func_select(self,functions,uin):
+    def func_select(self,functions,uin,thresh):
         finfun =None
         similarity=0
-        tInput = word_tokenize(uin)
+        tInput=uin
+        #tInput = word_tokenize(uin)
+        #tInput = self.stem_txt(tInput)
+        #tInput = self.remStopWords(tInput)
         vectors = self.model.wv
         for f in functions:
             for w in tInput:
                 if w in vectors:
                     nsim = self.model.similarity(f,w)
-                    if nsim>similarity and nsim>0.25:
+                    if nsim>similarity and nsim>thresh:
                         similarity = nsim
                         finfun = f
         return [finfun,similarity]
